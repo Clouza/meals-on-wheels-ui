fac2ed7dc15bdbdd8daee2e75322ab68341e3bd0
javines24 <javines2403@gmail.com>
2023-04-03 17:08:49 +0800

    just the donor form with no header and footer

---
+++ src/Pages/DonorPage.js
@@ -0,0 +1,15 @@
+import DonorForm from "../Component/Donor/DonorForm";
+import Footer from "../Component/Footer/Footer"
+import NavbarMember from "../Component/Navbar/NavbarMember"
+
+const DonorPage = () => {
+	return(
+		<>
+           
+			<DonorForm/>
+        
+		</>
+	)
+}
+
+export default DonorPage;
--- src/WebRouter.js
+++ src/WebRouter.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import LandingPage from './Pages/LandingPage';
+import DonorPage from './Pages/DonorPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage/>}></Route>
+      <Route path="/donate" element={<DonorPage/>}></Route>
     </Routes>
   );
 }
