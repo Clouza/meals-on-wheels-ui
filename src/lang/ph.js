c1516da06a1b9d5e2af4018ade3cf457a75f3e72
javines24 <javines2403@gmail.com>
2023-04-07 09:21:38 +0800

    added the ph language

---
+++ src/lang/ph.js
@@ -0,0 +1,37 @@
+const ph = {
+  // navbar
+  navbar_home: "Home",
+  navbar_donate: "Mag-donate",
+  navbar_about: "Tungkol sa Amin",
+  navbar_contact: "Makipag-ugnay",
+  
+  // footer
+  footer_information: "Impormasyon",
+  footer_newsletter: "Mag-subscribe sa Newsletter at Makakuha ng Pinakabagong Balita",
+  footer_guide: "Mabilis na Gabay",
+  footer_partnership: "Ang Aming Partnership",
+  
+  // donor
+  donor_header: "Maging Donor",
+  
+  // button
+  login: "Mag-login",
+  join: "Sumali",
+  subscribe: "Mag-subscribe",
+  
+  // additional
+  support: "Suporta",
+  
+  // form
+  name: "Pangalan",
+  email: "Email",
+  date: "Petsa",
+  comment: "Komento",
+  
+  // form input
+  name_input: "Ilagay ang iyong pangalan",
+  email_input: "Ilagay ang iyong email address",
+  comment_input: "Ilagay ang iyong mga komento"
+  }
+  
+  export default ph;
