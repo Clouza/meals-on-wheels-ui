e37981032836abd51bc086c32b6ac112d78d2613
javines24 <javines2403@gmail.com>
2023-04-11 12:02:50 +0800

    Updated Members Order

---
+++ src/components/member/MemberOrder.js
@@ -0,0 +1,85 @@
+import '../../css/partner/partner.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+import MemberFoodCard from './MemberFoodCard';
+
+
+
+const MemberOrder = () => {
+  const [activeTab, setActiveTab] = useState("currentOrder");
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
+
+  return (
+    <>
+      <div className='partner container mb-4'>
+      {/* Tabs navs */}
+      <ul className="nav nav-tabs mb-3" id="ex1" role="tablist">
+        <li className="nav-item" role="presentation">
+          <a
+            className={activeTab === "currentOrder" ? "nav-link active" : "nav-link"}
+            id="currentOrder"
+            data-mdb-toggle="tab"
+            href="#currentOrder"
+            role="tab"
+            aria-controls="currentOrder"
+            aria-selected={activeTab === "currentOrder"}
+            onClick={() => handleTabClick("currentOrder")}
+          >
+            Current Order
+          </a>
+        </li>
+        <li className="nav-item" role="presentation">
+          <a
+            className={activeTab === "history" ? "nav-link active" : "nav-link"}
+            id="history"
+            data-mdb-toggle="tab"
+            href="#history"
+            role="tab"
+            aria-controls="history"
+            aria-selected={activeTab === "history"}
+            onClick={() => handleTabClick("history")}
+          >
+            History
+          </a>
+        </li>
+      </ul>
+      {/* Tabs navs */}
+
+      {/* Tabs content */}
+      <div className="tab-content" id="currentOrder">
+        <div
+          className={
+            activeTab === "currentOrder"
+              ? "tab-pane fade show active"
+              : "tab-pane fade"
+          }
+          id="currentOrder"
+          role="tabpanel"
+          aria-labelledby="ex1-tab-1"
+        >
+          <MemberFoodCard/>
+        </div>
+        <div
+          className={
+            activeTab === "history"
+              ? "tab-pane fade show active"
+              : "tab-pane fade"
+          }
+          id="history"
+          role="tabpanel"
+          aria-labelledby="history"
+        >
+          <MemberFoodCard/>
+        </div>
+      </div>
+      {/* Tabs content */}
+      </div>
+    </>
+  );
+}
+
+
+export default MemberOrder
