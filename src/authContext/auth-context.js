9729123b34b5305aa785f85059fcbcb99338cc5f
romakelap <romakelapa833@gmail.com>
2023-04-17 18:05:09 +0800

    Add auth context

--- src/authContext/auth-context.js
+++ src/authContext/auth-context.js
@@ -1,14 +1,20 @@
-import axios from "axios";
+
 import React, { useEffect, useState } from "react";
+import Service from "../service/Service";
+import { useNavigate } from "react-router-dom";
 // Create Context API
 const AuthContext = React.createContext({
-  username: "",
-  email: "",
   userId: "",
   token: "",
+  user:{},
+  isAdmin: false,
+  isMember: false,
+  isRider: false,
+  isPartner: false,
   isLoggedIn: false,
-  login: (token) => { },
-  logout: () => { },
+  redirectLogin: (role) => {},
+  login: (token) => {},
+  logout: () => {},
 });
 
 // Retrive Stored Token From Local Storage
@@ -31,31 +37,57 @@ export const AuthContextProvider = (props) => {
 
 // USE STATE
 const [token, setToken] = useState(initialToken);
-const [user, setUser] = useState({
-  username: "",
-  email: "",
-  userId: "",
-});
+const [userId, setUserId] = useState("");
+const [role, setRole] = useState("");
+const [isAdmin, setIsAdmin] = useState(false);
+const [isMember, setIsMember] = useState(false);
+const [isRider, setIsRider] = useState(false);
+const [isPartner, setIsPartner] = useState(false);
+const [user, setUser] = useState({});
+const navigate = useNavigate();
 
 // Check Token (!! = convert to Boolean)
 const userIsLoggedIn = !!token;
 
 // GET USER LOGIN DATA
 useEffect(() => {
-  if (token) {((res) => {
-        setUser({
-          user: res.data.user,
-          email: res.data.email,
-          userId: res.data.id,
-        });
-      })
-      .catch((err) => {
-        console.log(err.message);
-        // Logout if the token is expired or incorrect
-        logoutHandler();
-      });
+  if (token) {
+    Service.getUser(token).then((res) => {
+      setUserId(res.data._id);
+      setRole(res.data.role);
+      setUser(res.data);
+    });
   }
-}, [token]);
+  if (role && role !== "") {
+    // MEMBER
+    if (role === "MEMBER") {
+      setIsMember(true);
+    } else {
+      setIsMember(false);
+    }
+
+    // RIDER
+    if (role === "RIDER") {
+      setIsRider(true);
+    } else {
+      setIsRider(false);
+    }
+
+    // PARTNER
+    if (role === "PARTNER") {
+      setIsPartner(true);
+    } else {
+      setIsPartner(false);
+    }
+
+    // ADMIN
+    if (role === "ADMIN") {
+      setIsAdmin(true);
+    } else {
+      setIsAdmin(false);
+    }
+  }
+}, [token,role]);
 
   // LOGOUT
   const logoutHandler = () => {
@@ -68,14 +100,33 @@ useEffect(() => {
     setToken(token);
     localStorage.setItem("token", token);
   };
+  const redirectHandler = (role) => {
+    if (role === "ADMIN") {
+      navigate("/admin");
+    }
+    if (role === "MEMBER") {
+      navigate("/member");
+    }
+    if (role === "RIDER") {
+      navigate("/rider");
+    }
+    if (role === "PARTNER") {
+      navigate("/partnerFoodList");
+    }
+  };
 
   // Context Value
   const contextValue = {
-    username: user.username,
-    email: user.email,
-    userId: user.userId,
     token: token,
+    userId: userId,
+    role: role,
+    user: user,
+    isAdmin: isAdmin,
+    isMember: isMember,
+    isRider: isRider,
+    isPartner: isPartner,
     isLoggedIn: userIsLoggedIn,
+    redirectLogin: redirectHandler,
     login: loginHandler,
     logout: logoutHandler,
   };
--- src/components/navbar/NavbarMember.js
+++ src/components/navbar/NavbarMember.js
@@ -1,7 +1,7 @@
 import React, { useState, useContext } from "react";
 import '../../css/landing.css';
 import { Link, useNavigate } from 'react-router-dom';
-import AuthContext from "../../authContext/auth-context"
+import AuthContext from "../../authContext/auth-context";
 
 // Import icon
 import { SiConsul, SiSessionize } from 'react-icons/si';
@@ -17,11 +17,13 @@ import lang from '../../Localization';
 
 const NavbarMember = () => {
 
+
     // For auth
     const authCtx = useContext(AuthContext);
     const navigate = useNavigate();
     const logoutHandler = () => {
         authCtx.logout();
+        sessionStorage.removeItem('token');
         navigate("/login");
     };
     // ===========================
@@ -63,8 +65,8 @@ const NavbarMember = () => {
     return (
         <div className="navBar flex">
             {/* Header user not yet login */}
-            {!authCtx.isLoggedIn && (<>
-                <div className="navBarOne flex">
+            {/* {!authCtx.isLoggedIn && (<> */}
+            <div className="navBarOne flex">
                 <div>
                     <SiConsul className="icon" />
                 </div>
@@ -82,6 +84,19 @@ const NavbarMember = () => {
                 <div className="atb flex">
                     {/* <span>User</span> */}
                 </div>
+
+                {sessionStorage.getItem('token') != null ?
+                    <div className="atb flex">
+                        <span>WELCOME "name user"</span>
+                    </div>
+                    :
+                    // else
+                    <>
+                        <div className="atb flex">
+                            {/* <span>User</span> */}
+                        </div>
+                    </>
+                }
             </div>
 
             <div className={noBg} >
@@ -112,18 +127,29 @@ const NavbarMember = () => {
                         </button>
                     </Link>
                 </div>
-                <Link to="/login">
-                    <button style={styles} className="btn flex btnoutThree">
-                        Login
-                    </button>
-                </Link>
+                {sessionStorage.getItem('token') != null ?
+                    <Link to="/">
+                        <button style={logout} onClick={logoutHandler} className="btn flex btnThree">
+                            Logout
+                        </button>
+                    </Link>
+                    :
+                    // else
+                    <>
+                        <Link to="/login">
+                            <button style={styles} className="btn flex btnoutThree">
+                                Login
+                            </button>
+                        </Link>
 
 
-                <Link to="/register">
-                    <button style={styles} className="btn flex btnThree">
-                        Join
-                    </button>
-                </Link>
+                        <Link to="/register">
+                            <button style={styles} className="btn flex btnThree">
+                                Join
+                            </button>
+                        </Link>
+                    </>
+                }
 
 
                 <div onClick={showNavBar} className="toggleIcon">
@@ -131,72 +157,11 @@ const NavbarMember = () => {
                 </div>
 
             </div>
-            </>)}
+            {/* </>)} */}
 
 
             {/* ==================================================== */}
 
-
-             {/* Header login */}
-             {authCtx.isLoggedIn && (<>
-                <div className="navBarOne flex">
-                <div>
-                    <SiConsul className="icon" />
-                </div>
-
-                <div className="none flex">
-                    <li className="flex">
-                        <BsPhoneVibrate /> Support
-                    </li>
-                    <li className="flex select-none" onClick={localization}>
-                        <AiOutlineGlobal /> {sessionStorage.getItem('language')} <IoIosArrowDown />
-                        {isChanged ? <Locale /> : ""}
-                    </li>
-                </div>
-
-                <div className="atb flex">
-                    <span>User</span>
-                </div>
-            </div>
-
-            <div className={noBg} >
-
-                <div className="logoDiv">
-                    <img src={logo} className="logo" />
-                </div>
-
-                <div className={active}>
-                    <ul className="menu flex">
-                        <li onClick={removeNavBar} className="listItem"> <a href="/"> {lang.navbar_home}</a> </li>
-                        <li onClick={removeNavBar} className="listItem"> <a href="/donate">{lang.navbar_donate} </a> </li>
-                        <li onClick={removeNavBar} className="listItem"><a href="/about">{lang.navbar_about}</a></li>
-                        <li onClick={removeNavBar} className="listItem"><a href="/contact">{lang.navbar_contact}</a>  </li>
-                        <li onClick={removeNavBar} className="listItem">  </li>
-                    </ul>
-                    <Link to="/register">
-                        <button style={logout} onClick={() => {removeNavBar(); logoutHandler();}}  className="btn flex btnOne">
-                            Logout
-                        </button>
-                    </Link>
-                </div>
-                <Link to="/register">
-                    <button style={logout} onClick={logoutHandler} className="btn flex btnThree">
-                        Logout
-                    </button>
-                </Link>
-
-
-                <div onClick={showNavBar} className="toggleIcon">
-                    <CgMenuGridO className="icon" />
-                </div>
-
-            </div>
-            </>)}
-
-
-
-
-            
 
         </div>
     )
